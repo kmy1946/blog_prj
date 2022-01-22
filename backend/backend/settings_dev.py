7fbcc9eca854f8b9b80a684ac3ec03fd2d6fef74
kmy1946 <kmy1946@outlook.jp>
2022-01-22 15:10:30 +0900

    settings_dev_common_commit

---
+++ backend/backend/settings_dev.py
@@ -0,0 +1,36 @@
+from .settings import *
+#SECURITY WARNING: don't run with debug turned on in production!
+DEBUG=True
+ALLOWED_HOSTS = []
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'level':'INFO',
+        },
+        'backend':{
+            'handlers':['console'],
+            'level':'DEBUG',
+        },
+    },
+    "handlers": {
+        'console': {
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter':'dev'
+        },
+    },
+    "formatters": {
+        "dev": {
+            "format": '\t'.join([
+                "%(asctime)s",
+                "[%(levelname)s]",
+                "%(pathname)s(Line:%(Lineno)d)",
+                "%(message)s"
+                ])
+            },
+        }
+    }
